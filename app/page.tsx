af080cbc466e4de2985c5f3df6f4763b287ba995
Anurag-Kochar-1 <anuragmarketing101@gmail.com>
2024-02-11 22:39:41 +0530

    getting started 🤘

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-neutral-800">
+      <h1 className="text-5xl animate-spin"> 💿 </h1>
+    </main>
+  );
+}
