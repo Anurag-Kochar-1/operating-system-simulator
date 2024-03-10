8ecfd363a7a540d7b860be8940d0502aaf06c1c7
Anurag-Kochar-1 <anuragmarketing101@gmail.com>
2024-03-10 22:23:37 +0530

    🛠  revise: resume link

--- components/apps/resume.tsx
+++ components/apps/resume.tsx
@@ -4,7 +4,7 @@ export const Resume = () => {
   return (
     <div className="flex h-full w-full flex-col items-center justify-start gap-6 overflow-y-auto">
       <iframe
-        src="https://docs.google.com/file/d/1JqBoP--acA31oecpS-aSUAsDmw1gjQ2c/preview?usp=sharing"
+        src="https://docs.google.com/file/d/1wXPzS7NYn4tsVK4vxlXsN1BcmS8tkyvH/preview?usp=sharing"
         className="h-full w-full"
         allowFullScreen
       ></iframe>
