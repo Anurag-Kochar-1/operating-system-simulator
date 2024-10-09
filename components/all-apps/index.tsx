abc1515b803a93b32f84ffd6c580227a8401ca6d
anurag-kochar <anuragmarketing101@gmail.com>
2024-10-10 05:29:51 +0530

    ✨ feat: minimize / maximize window

--- components/all-apps/index.tsx
+++ components/all-apps/index.tsx
@@ -1,26 +1,14 @@
 "use client";
 import React, { useEffect } from "react";
 import { useApp } from "@/hooks/use-app";
-import { useTheme } from "next-themes";
 
 export const AllApps = () => {
-  const { setTheme } = useTheme();
   const { apps, windows, addWindow, setFocusedWindow } = useApp();
   useEffect(() => {
     const recentlyAddedWindow = windows[windows?.length - 1];
     setFocusedWindow(recentlyAddedWindow);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [windows]);
-
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     addWindow({ id: "about", title: "About me", type: "APP" });
-  //   }, 1000);
-  //   return () => {
-  //     clearTimeout(timer);
-  //   };
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
   return (
     <div className="flex h-max w-[90%] flex-wrap items-start justify-start gap-10 p-4 md:h-[93vh] md:w-min md:flex-col">
       {apps
