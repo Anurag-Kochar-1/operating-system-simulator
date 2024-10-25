09c1edbf436812db17514630af5620d8747c24c6
anurag-kochar <anuragmarketing101@gmail.com>
2024-10-25 14:03:14 +0530

    ⚒  update: public resume link with doc predix && re-added loading and error handling

--- components/apps/resume.tsx
+++ components/apps/resume.tsx
@@ -1,32 +1,34 @@
-import React from "react";
+"use client"
+import { Loader2 } from "lucide-react";
+import React, { useState } from "react";
 
 export const ResumeAppContent = () => {
-  // const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  // const handleLoad = () => {
-  //   setTimeout(() => {
-  //     setIsLoading(false);
-  //   }, 5000);
-  // };
+  const handleLoad = () => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 5000);
+  };
 
-  // const handleError = (e: React.SyntheticEvent<HTMLIFrameElement, Event>) => {
-  //   console.error("Failed to load resume:", e);
-  //   setIsLoading(false);
-  // };
+  const handleError = (e: React.SyntheticEvent<HTMLIFrameElement, Event>) => {
+    console.error("Failed to load resume:", e);
+    setIsLoading(false);
+  };
 
   return (
     <div className="relative flex h-full w-full flex-col items-center justify-start overflow-hidden">
-      {/* {isLoading && (
+      {isLoading && (
         <div className="absolute inset-0 flex items-center justify-center z-10">
           <Loader2 size={40} className="animate-spin text-blue-500" />
         </div>
-      )} */}
+      )}
       <iframe
-        src="https://drive.google.com/file/d/1mJUiJFbtpcV5Bm7cG-fW6ndTL77Z45Ba/view?usp=sharing"
+        src="https://docs.google.com/file/d/1mJUiJFbtpcV5Bm7cG-fW6ndTL77Z45Ba/preview?usp=sharing"
         className="h-full w-full border-none"
         allowFullScreen
-        // onLoad={handleLoad}
-        // onError={handleError}
+        onLoad={handleLoad}
+        onError={handleError}
         title="Resume Preview"
       />
     </div>
