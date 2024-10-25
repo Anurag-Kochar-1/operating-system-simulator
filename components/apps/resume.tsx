c7fb5ce210b40e9df2563ce8008175668b24b286
anurag-kochar <anuragmarketing101@gmail.com>
2024-10-25 13:57:33 +0530

    ⚒  update: public resume link && remove loading state from resume app

--- components/apps/resume.tsx
+++ components/apps/resume.tsx
@@ -1,34 +1,32 @@
-"use client"
-import { Loader2 } from "lucide-react";
-import React, { useState } from "react";
+import React from "react";
 
 export const ResumeAppContent = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  // const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  const handleLoad = () => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 5000);
-  };
+  // const handleLoad = () => {
+  //   setTimeout(() => {
+  //     setIsLoading(false);
+  //   }, 5000);
+  // };
 
-  const handleError = (e: React.SyntheticEvent<HTMLIFrameElement, Event>) => {
-    console.error("Failed to load resume:", e);
-    setIsLoading(false);
-  };
+  // const handleError = (e: React.SyntheticEvent<HTMLIFrameElement, Event>) => {
+  //   console.error("Failed to load resume:", e);
+  //   setIsLoading(false);
+  // };
 
   return (
     <div className="relative flex h-full w-full flex-col items-center justify-start overflow-hidden">
-      {isLoading && (
+      {/* {isLoading && (
         <div className="absolute inset-0 flex items-center justify-center z-10">
           <Loader2 size={40} className="animate-spin text-blue-500" />
         </div>
-      )}
+      )} */}
       <iframe
-        src="https://drive.google.com/file/d/1AEQ8C-hp886sEoNOsLmAIjOdD5VK7trK/view?usp=sharing"
+        src="https://drive.google.com/file/d/1mJUiJFbtpcV5Bm7cG-fW6ndTL77Z45Ba/view?usp=sharing"
         className="h-full w-full border-none"
         allowFullScreen
-        onLoad={handleLoad}
-        onError={handleError}
+        // onLoad={handleLoad}
+        // onError={handleError}
         title="Resume Preview"
       />
     </div>
